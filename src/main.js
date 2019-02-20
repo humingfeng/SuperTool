8bced5a9ca30359e0d425123c7efea76e8da0b52
humingfeng <humf@vip.qq.com>
2019-02-20 19:15:09 +0800

    #####

--- src/main.js
+++ src/main.js
@@ -1,19 +1,47 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+//import NProgress from 'nprogress'
+//import 'nprogress/nprogress.css'
+import routes from './routes'
+import Mock from './mock'
+Mock.bootstrap();
+import 'font-awesome/css/font-awesome.min.css'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(Vuex)
 
-Vue.config.productionTip = false
+//NProgress.configure({ showSpinner: false });
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+const router = new VueRouter({
+  routes
 })
+
+router.beforeEach((to, from, next) => {
+  //NProgress.start();
+  if (to.path == '/login') {
+    sessionStorage.removeItem('user');
+  }
+  let user = JSON.parse(sessionStorage.getItem('user'));
+  if (!user && to.path != '/login') {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
+})
+
+//router.afterEach(transition => {
+//NProgress.done();
+//});
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
