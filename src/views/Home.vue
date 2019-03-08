1b94cbfc42ca35f1fe9a8af000db4dd1ffd90e96
humingfeng <humf@vip.qq.com>
2019-03-08 16:04:05 +0800

    #日常提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,7 +21,7 @@
             </el-submenu>
           </el-submenu>
           <el-menu-item index="3" disabled>消息中心</el-menu-item>
-          <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+          <!--<el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>-->
         </el-menu>
 			</el-col>
 		</el-col>
